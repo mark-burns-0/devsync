82d64335a0bbc2efe486fcd76f64820c839b071c
jhondoe <margar.melkonyan@bk.ru>
2026-05-01 22:09:18 +0300

    chore: add .env.example

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/mark-burns-0/devsync
 
 go 1.24.10
+
+require (
+	github.com/k0kubun/pp/v3 v3.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+)
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,14 +2,45 @@ package app
 
 import (
 	"fmt"
-	"os/exec"
+	"io/fs"
+	"path/filepath"
+	"strings"
+
+	"github.com/k0kubun/pp/v3"
 )
 
+var root = ""
+var gitDir = ".git"
+
 func Run() {
-	cmd := exec.Command("ls")
-	cmd.Dir = "."
-	err := cmd.Run()
+	dirs := []string{}
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() && d.Name() == ".git" {
+			path := strings.TrimSuffix(strings.TrimPrefix(path, root), gitDir)
+			cleanPath := strings.Trim(path, string(filepath.Separator))
+			dirs = append(dirs, cleanPath)
+			return fs.SkipDir
+		}
+
+		return nil
+	})
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Ошибка при поиске: %v\n", err)
 	}
+
+	pp.Print(dirs)
+
+	// cmd := exec.Command("ls", "-la")
+	// cmd.Dir = "."
+	// cmd.Stdout = os.Stdout
+
+	// err = cmd.Run()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 }
