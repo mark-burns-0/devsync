11712eb32d8bb038be20c0bfe19bf1c63d6bdd8d
jhondoe <margar.melkonyan@bk.ru>
2026-05-01 22:24:57 +0300

    feat: add configuration loading success message to main and app.Run

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/mark-burns-0/devsync/internal/app"
 	"github.com/mark-burns-0/devsync/internal/config"
 )
@@ -10,6 +12,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Configuration loaded successfully")
 
 	app.Run(cfg)
 }
